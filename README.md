1c46cd0aa450ad4cb1d0f0b458df8ed4266c26cb
Jack Jackson <scubbojj@gmail.com>
2025-03-11 20:12:20 -0700

    Acceptable solution to 2530

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 These solutions were intentionally reached _without_ using AI Dev Tools - although I've become a firm convert to Cursor over the last couple of weeks, I still think it's important to hone the underlying skills so that I can _evaluate_ the output of an AI Tool.
+
+# TODOs
+
+Questions where I have more-efficient ideas to return to.
+
+* 2530 - implement and use a min-heap
--- solutions/2530.ts
+++ solutions/2530.ts
@@ -1,6 +1,8 @@
-// Naive approach - just go through sequentially picking the highest number each time
-function maxKelements(nums: number[], k: number): number {
-    let score = 0
+// Naive approach - just go through sequentially picking the highest number each time (I think the "best" approach would
+// be to use a MinHeap, but there's no built-in implementation in TypeScript, so I'll try simpler solutions until/unless
+// that performance is unacceptable)
+function maxKelementsNaive(nums: number[], k: number): number {
+    let score = 0;
     for (let iteration = 0; iteration < k; iteration++) {
         let largest = 0; // Array is positive
         let largest_index = -1;
@@ -16,5 +18,87 @@ function maxKelements(nums: number[], k: number): number {
     return score
 };
 
-// The more sophisticated approach will, I think, be to create a maxHeap of size k, and always pop-and-repush the
+// Slightly more sophisticated:
+// * Descending-Sort the array
+// * Take the first value, then reinsert it's lowered value at the appropriate place in the array
+function maxKelementsBetter(nums: number[], k: number): number {
+    let nums_sorted = nums.sort((a, b) => b - a)
+    let score = 0;
+    for (let iteration = 0; iteration < k; iteration++) {
+        score += nums_sorted[0]
+        const lowered_highest = Math.ceil(nums_sorted[0] / 3)
+        let idx = 0;
+        while (nums_sorted[idx+1] > lowered_highest) {
+            nums_sorted[idx] = nums_sorted[idx+1]
+            idx++;
+        }
+        nums_sorted[idx] = lowered_highest
+    }
+    return score;
+}
+// Hmm, the approach above _still_ timed out. Guess we're implementing a Min Heap after all!
+
+class MaxHeap {
+    data: number[]
+    constructor(data: number[] = []) {
+        this.data = [...data];
+        this.buildHeap()
+    }
+
+    buildHeap() {
+        for (let i = Math.floor(this.data.length / 2) - 1; i >= 0; i--) {
+            this.heapify(i)
+        }
+    }
+
+    // Turn the sub-tree rooted at index i into a minheap
+    heapify(i: number): void {
+        const left = i*2+1
+        const right = i*2+2
+        let largest = i;
+        if (this.data[i] < this.data[left]) {
+            largest = left
+        }
+        if (this.data[largest] < this.data[right]) {
+            largest = right
+        }
+        if (largest != i) {
+            this.swap(i, largest)
+            this.heapify(largest)
+        }
+    }
+
+    swap(a: number, b: number): void {
+        const temp = this.data[b]
+        this.data[b] = this.data[a]
+        this.data[a] = temp
+    }
+
+    popAndRepush(f: (i: number) => number): number {
+        const origValue = this.data[0];
+        this.data[0] = f(origValue)
+        this.heapify(0)
+        return origValue
+    }
+}
+
+function maxKelements(nums: number[], k: number): number {
+    const heap = new MaxHeap(nums)
+    let score = 0;
+    for (let iteration = 0; iteration < k; iteration++) {
+        score += heap.popAndRepush((i) => Math.ceil(i/3))
+    }
+    return score
+}
+
+if (maxKelements([1, 10, 3, 3, 3], 3) != 17) {
+    throw new Error("failed!")
+} else {
+    console.log("Passed")
+}
+
+// The best approach, I think, be to create a maxHeap of size k, and always pop-and-repush the
 // biggest number (repushing the divided-by-three value)
+//
+// ...but there's no MinHeap implementation in TypeScript, so I may return to this with a build-your-own implementation
+// of a MinHeap at some point.

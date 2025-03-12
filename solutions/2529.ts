28acb14bb613a8480630ca3dbb2e03e2e7b62fee
Jack Jackson <scubbojj@gmail.com>
2025-03-11 19:00:54 -0700

    Smarter solution to 2529

--- solutions/2529.ts
+++ solutions/2529.ts
@@ -1,5 +1,5 @@
 // First attempt will just do it naively, without taking advantage of the non-decreasing nature of the array.
-function maximumCount(nums: number[]): number {
+function maximumCountNaive(nums: number[]): number {
     let countPos = 0
     let countNeg = 0
     nums.forEach((i) => {
@@ -11,4 +11,30 @@ function maximumCount(nums: number[]): number {
         }
     })
     return Math.max(countPos, countNeg)
+}
+
+// Smarter attempt
+function maximumCount(nums: number[]): number {
+    let idx = 0
+    while (nums[idx] < 0) {
+        idx += 1
+        if (idx == nums.length) {
+            break
+        }
+    }
+    const countNeg = idx
+    let countPos: number
+    if (idx == nums.length) {
+        countPos = 0
+    } else {
+        while (nums[idx] == 0) {
+            idx += 1
+            if (idx == nums.length) {
+                break
+            }
+        }
+        countPos = nums.length - idx
+    }
+
+    return Math.max(countNeg, countPos);
 }
